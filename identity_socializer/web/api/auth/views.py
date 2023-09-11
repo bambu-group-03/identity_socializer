4b5d4932b4f28db8dfc2029b1d7627877e427625
Mafer <mf.pont.tv@gmail.com>
2023-09-11 19:29:12 -0300

    signup error bad JWT

--- identity_socializer/web/api/auth/views.py
+++ identity_socializer/web/api/auth/views.py
@@ -3,10 +3,26 @@ router = APIRouter()
 from identity_socializer.web.api.auth.schema import Signup
 from identity_socializer.web.api.auth.schema import SecurityToken
 
+
 @router.post("/signup", response_model=SecurityToken) 
 async def signup(
     incoming_message: Signup,
 ) -> SecurityToken:
   assert incoming_message.username 
-  ret = SecurityToken(token="my token super secreta")
+
+  import firebase_admin
+  from firebase_admin import credentials
+  from firebase_admin import auth
+  cred = credentials.Certificate("identity_socializer/firebase_credentials.json")
+  firebase_admin.initialize_app(cred)
+
+  user = auth.create_user(
+    email=incoming_message.email,
+    email_verified=False,
+    password=incoming_message.password,
+    disabled=False)
+  print('Sucessfully created new user: {0}'.format(user.uid))
+
+
+  ret = SecurityToken("Juanito alimaña")
   return ret
