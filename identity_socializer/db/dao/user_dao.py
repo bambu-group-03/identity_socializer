5612213dd44db9ce8ebc6c9caa1245a21bc315a0
Nox <davela@fi.uba.ar>
2023-10-15 00:54:53 -0300

    fix types

--- identity_socializer/db/dao/user_dao.py
+++ identity_socializer/db/dao/user_dao.py
@@ -18,10 +18,10 @@ class UserDAO:
         self,
         uid: str,
         email: str,
-        first_name: str | None = None,
-        last_name: str | None = None,
-        phone_number: str | None = None,
-        bio_msg: str | None = None,
+        first_name: (str | None) = None,
+        last_name: (str | None) = None,
+        phone_number: (str | None) = None,
+        bio_msg: (str | None) = None,
     ) -> None:
 
         """
