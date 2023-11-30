ad5c8c3952d36637ceb0e9f32d8bb5a22b19a050
Nox <davela@fi.uba.ar>
2023-11-30 00:14:43 -0300

    Add test for updating user information

--- identity_socializer/tests/test_auth.py
+++ identity_socializer/tests/test_auth.py
@@ -35,3 +35,53 @@ async def test_register(
 
     assert instance is not None
     assert instance.email == test_email
+
+
+@pytest.mark.anyio
+async def test_update_user(
+    fastapi_app: FastAPI,
+    client: AsyncClient,
+    dbsession: AsyncSession,
+) -> None:
+    """Tests update user."""
+    dao = UserDAO(dbsession)
+
+    # Create user
+    test_id = uuid.uuid4().hex
+    test_email = f"{test_id}@gmail.com"
+
+    await dao.create_user_model(
+        uid=test_id,
+        email=test_email,
+    )
+
+    # Update user
+    url = fastapi_app.url_path_for("update_user")
+
+    response = await client.put(
+        url,
+        json={
+            "id": test_id,
+            "first_name": "first_name_test",
+            "last_name": "last_name_test",
+            "username": "username_test",
+            "phone_number": "phone_number_test",
+            "bio_msg": "bio_msg_test",
+            "profile_photo_id": "profile_photo_id_test",
+            "ubication": "Argentina",
+        },
+    )
+
+    instance = await dao.get_user_by_id(user_id=test_id)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    assert instance is not None
+    assert instance.email == test_email
+    assert instance.first_name == "first_name_test"
+    assert instance.last_name == "last_name_test"
+    assert instance.username == "username_test"
+    assert instance.phone_number == "phone_number_test"
+    assert instance.bio_msg == "bio_msg_test"
+    assert instance.profile_photo_id == "profile_photo_id_test"
+    assert instance.ubication == "Argentina"
