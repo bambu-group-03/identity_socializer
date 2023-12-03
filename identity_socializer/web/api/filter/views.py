ea8db96fa0b0099246be8936f396b280ae99445f
Nox <davela@fi.uba.ar>
2023-12-02 21:24:58 -0300

    Refactor admin_filter_user endpoint to use UserDAO's filter_user method

--- identity_socializer/web/api/filter/views.py
+++ identity_socializer/web/api/filter/views.py
@@ -4,20 +4,20 @@ from fastapi import APIRouter, Depends
 
 from identity_socializer.db.dao.relationship_dao import RelationshipDAO
 from identity_socializer.db.dao.user_dao import UserDAO
+from identity_socializer.db.models.user_model import UserModel
 from identity_socializer.web.api.auth.schema import AppUserModel
 from identity_socializer.web.api.utils import complete_users
 
 router = APIRouter()
 
 
-@router.get("/{username}", response_model=None)
+@router.get("/admin/{username}", response_model=None)
 async def admin_filter_user(
     username: str,
     user_dao: UserDAO = Depends(),
-    relationship_dao: RelationshipDAO = Depends(),
-) -> List[AppUserModel]:
+) -> List[UserModel]:
     """Retrieve a filtered users."""
-    return await filter_user(username, "unknown", user_dao, relationship_dao)
+    return await user_dao.filter_user(username)
 
 
 @router.get("/{current_user_id}/{username}", response_model=None)
